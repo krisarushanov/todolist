ffbd651028d45eb560acda7303703366d62fd776
Kristena Arushanov <kris.arushanov@gmail.com>
2021-04-21 21:59:33 -0700

    updated engines with npm version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoList from "./TodoList";
+import TodoList from "./TodoList.js";
 
 import reportWebVitals from './reportWebVitals';
 
