51e3cf77d4f16927f00a9fb0547160a2ebae5924
Kristena Arushanov <kris.arushanov@gmail.com>
2021-07-22 09:15:10 -0700

    minor styling/rename list title

--- src/Title.js
+++ src/Title.js
@@ -5,7 +5,7 @@ import './Title.css';
 function Title() {
     return (
       <div className="Title">
-        <h1>Epic To-Do List</h1>
+        <h1>GET IT DONE</h1>
               
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoList from "./TodoList";
+import TodoList from "./ToDoList";
 import Title from './Title';
 
 import reportWebVitals from './reportWebVitals';
